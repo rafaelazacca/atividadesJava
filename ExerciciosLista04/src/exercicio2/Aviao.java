42fa6bd95e45c2759c4e792c7f663c0d309e7288
Rafaela <rafaelazaccarias@gmail.com>
2021-05-10 23:26:40 -0300

    add exercicio 02

--- ExerciciosLista04/src/exercicio2/Aviao.java
+++ ExerciciosLista04/src/exercicio2/Aviao.java
@@ -1,28 +1,102 @@
 package exercicio2;
 
+import java.util.Scanner;
+
 public class Aviao {
 
+	// Atributos
+	private String modelo; 
+	private int velocidade;
+	private int distancia;
+	private int qtdeLugares;
 	
-	String modelo; 
-	int velocidade;
-	int distancia;
-	int qtdeLugares;
+	
+	
+	public Aviao(String modelo, int velocidade, int distancia, int qtdeLugares) {
+		
+		super();
+		this.modelo = modelo;
+		this.velocidade = velocidade;
+		this.distancia = distancia;
+		this.qtdeLugares = qtdeLugares;
+	}
+
+
+	//Encapsulamento
+	public String getModelo() {
+		return modelo;
+	}
+
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+
+	public int getVelocidade() {
+		return velocidade;
+	}
+
+
+
+	public void setVelocidade(int velocidade) {
+		this.velocidade = velocidade;
+	}
+
+
+
+	public int getDistancia() {
+		return distancia;
+	}
+
+
+
+	public void setDistancia(int distancia) {
+		this.distancia = distancia;
+	}
+
+
+
+	public int getQtdeLugares() {
+		return qtdeLugares;
+	}
+
+
+
+	public void setQtdeLugares(int qtdeLugares) {
+		this.qtdeLugares = qtdeLugares;
+	}
 	
 	
 	//Métodos / Ações 
+	public void cadastro() {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Digite o modelo do avião: ");
+		modelo = leia.next();
+		
+		System.out.println("Digite a velocidade do avião: ");
+		velocidade = leia.nextInt();
+		
+		System.out.println("Digite a distância percorrida pelo avião: ");
+		distancia = leia.nextInt();
+		
+		System.out.println("Digite a quantidade de lugares do avião: ");
+		qtdeLugares = leia.nextInt();
+		
+		
+	}
 	
-	void mostrarVelocidade() {
+		public void imprimirDados() {
 		
-	}
-	void mostrarQtdeLugares() {
+		System.out.println("O modelo do avião é: " + modelo);
+		System.out.println("A velocidade do avião é: " + velocidade);
+		System.out.println("A distância percorrida pelo avião é: " + distancia);
+		System.out.println("A quantidade de lugares do avião é: " + qtdeLugares);
 		
-	}
-	void informarModelo()
-	{
-		
-	}
-	void calcularDistancia()
-	{
-		
-	}
+		}
+	
 }
--- ExerciciosLista04/src/exercicio2/TestaAviao.java
+++ ExerciciosLista04/src/exercicio2/TestaAviao.java
@@ -4,36 +4,15 @@ package exercicio2;
 
 public class TestaAviao {
 
-	
-
+	public static void main(String[] args) {
+				
+		//Instanciando um objeto
+			
+		Aviao aviao = new Aviao(null, 0, 0, 0);
 		
-		public static void main(String[] args) {
-				
-				//Instanciando um objeto
-				
-				Aviao av = new Aviao();
-				
-				
-				av.modelo = "Boing";
-				av.distancia = 1000;
-				av.velocidade = 3000;
-				av.qtdeLugares = 100;
-				 
-				
-				av.informarModelo();
-				av.mostrarVelocidade();
-				av.calcularDistancia();
-				av.mostrarQtdeLugares();
-				
-				
-				
-				
-				System.out.println(av.modelo);
-				System.out.println(av.qtdeLugares);	
-				System.out.println(av.velocidade);
-				System.out.println(av.distancia);
-				
-
+		aviao.cadastro();
+		aviao.imprimirDados();
+		
 			}
 
 
